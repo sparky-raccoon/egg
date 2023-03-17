d7e914ebc586ff3b1de906f2f625211f5a9e0ada
sparky-raccoon <andrea.saezbou@gmail.com>
2023-03-17 18:02:19 +0100

    feat: add analytics to track visitors

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,9 @@
+import { Analytics } from '@vercel/analytics/react';
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Component {...pageProps} />
+    <Analytics />
+  </>
 }
